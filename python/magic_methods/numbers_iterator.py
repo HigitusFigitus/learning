d1b279a9ccba466e3e16b68a60e263dded08734c
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-11-25 00:17:10 -0800

    Add nums iterator

---
+++ python/magic_methods/numbers_iterator.py
@@ -0,0 +1,19 @@
+class NumbersIterator(object):
+    def __init__(self):
+        self.next_number = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        n = self.next_number
+        self.next_number += 1
+        return n
+
+    next = __next__
+
+
+iterator = iter(NumbersIterator())
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
