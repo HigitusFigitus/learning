b7cc59434f7f76780c0a5802e764842c296a8087
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-12-01 14:17:10 -0800

    Add csv parser with generator

---
+++ python/functional_programming/csv_iterator_generator/football_explorer/explorer.py
@@ -0,0 +1,23 @@
+import csv
+from .models import Player
+
+
+class FootballExplorer(object):
+    def __init__(self, csv_file_name):
+        self.csv_file_name = csv_file_name
+
+    def all(self):
+        with open(self.csv_file_name) as csvfile:
+            for row in csv.reader(csvfile):
+                yield Player(*row)
+
+    def search(self, country=None, year=None, age=None, position=None):
+        if not country and not year and not age and not position:
+            raise ValueError('Provide at least a valid argument:\
+                             country, year, age, and/or position.')
+        for player in self.all():
+            if ((not country or player.country == country)
+                and (not year or player.year == year)
+                and (not age or player.date_of_birth == age)
+                and (not position or player.position == position)):
+                    yield player
